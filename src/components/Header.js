ea6180abc192ba090d4c5c6594c1246a6a1e288b
D-sai-rohith <dhanukondasairohith@gmail.com>
2024-08-20 16:30:03 +0530

    search

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,6 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <div className='text-center shadow p-3'><h1>Contact Manger</h1></div>
+    <div className='text-center shadow p-3'><h1>Contact Manager</h1></div>
   );
 }
